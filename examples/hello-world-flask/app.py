4f38a1b2836ca450b5f19fc51f0a0a8cb0a535f8
Emily Ashley <15912063+emilyashley@users.noreply.github.com>
2022-11-22 15:20:54 -0600

    Add manual tracing to hello world route.

--- examples/hello-world-flask/app.py
+++ examples/hello-world-flask/app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from opentelemetry import trace
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    with trace.get_tracer(__name__).start_as_current_span("foo"):
+        with trace.get_tracer(__name__).start_as_current_span("bar"):
+            print("baz")
     return "Hello, World!"
