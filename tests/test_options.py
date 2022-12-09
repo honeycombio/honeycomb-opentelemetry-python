4811982edc622d420c5b19779f59153c43e57c40
Mike Goldsmth <goldsmith.mike@gmail.com>
2022-12-09 16:29:03 +0000

    tidy up options tests

--- tests/test_options.py
+++ tests/test_options.py
@@ -10,11 +10,13 @@ CLASSIC_APIKEY = "this is a string that is 32 char"
 APIKEY = "an api key for 22 char"
 
 
-def test_defaults():
+def test_defaults(monkeypatch):
+    monkeypatch.delenv(HONEYCOMB_API_KEY, raising=False)
+    monkeypatch.delenv(OTEL_SERVICE_NAME, raising=False)
     options = HoneycombOptions()
-    # assert options.apikey == None
-    # assert options.endpoint == "api.honeycomb.io:443"
-    # assert options.service_name == "unknown_service:python"
+    assert options.apikey == None
+    assert options.endpoint == "api.honeycomb.io:443"
+    assert options.service_name == "unknown_service:python"
 
 
 def test_can_set_service_name_with_param(monkeypatch):
