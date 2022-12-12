78906f9e43afc70a25c707e6ef3d685b77ac2c4e
Mike Goldsmth <goldsmith.mike@gmail.com>
2022-12-12 12:07:53 +0000

    remove unused distro tests

--- tests/test_distro.py
+++ tests/test_distro.py
@@ -13,8 +13,6 @@ from opentelemetry.metrics import get_meter_provider
 from opentelemetry.trace import get_tracer_provider
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 
-from pkg_resources import DistributionNotFound, require
-
 
 # classic keys are 32 chars long
 CLASSIC_APIKEY = "this is a string that is 32 char"
@@ -40,36 +38,3 @@ def test_distro_configure_defaults(monkeypatch):
 
     meter_provider = get_meter_provider()
     assert len(meter_provider._meters) == 0
-
-
-def test_can_set_service_name_with_param(monkeypatch):
-    monkeypatch.delenv(OTEL_SERVICE_NAME, raising=False)
-    configure_opentelemetry(service_name="my-service")
-    tracer_provider = get_tracer_provider()
-    assert tracer_provider._resource._attributes["service.name"] == "my-service"
-
-
-# def test_can_set_endpoint_with_param(monkeypatch):
-#     monkeypatch.delenv(OTEL_EXPORTER_OTLP_ENDPOINT, raising=False)
-#     configure_opentelemetry(endpoint='localhost:4317')
-#     assert os.environ.get(OTEL_EXPORTER_OTLP_ENDPOINT) == 'localhost:4317'
-
-
-# def test_can_set_endpoint_with_envvar(monkeypatch):
-#     monkeypatch.setenv(OTEL_EXPORTER_OTLP_ENDPOINT, "localhost:4317")
-#     configure_opentelemetry()
-#     assert os.environ.get(OTEL_EXPORTER_OTLP_ENDPOINT) == 'localhost:4317'
-
-
-# def test_can_set_apikey_with_param(monkeypatch):
-#     monkeypatch.delenv(HONEYCOMB_API_KEY, raising=False)
-#     configure_opentelemetry(apikey=APIKEY)
-#     assert os.environ.get(
-#         OTEL_EXPORTER_OTLP_HEADERS) == f'x-honeycomb-team={APIKEY}'
-
-
-# def test_can_set_apikey_with_envvar(monkeypatch):
-#     monkeypatch.setenv(HONEYCOMB_API_KEY, APIKEY)
-#     configure_opentelemetry()
-#     assert os.environ.get(
-#         OTEL_EXPORTER_OTLP_HEADERS) == f'x-honeycomb-team={APIKEY}'
