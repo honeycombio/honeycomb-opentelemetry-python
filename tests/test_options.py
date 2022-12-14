0571820cb6db440460bcfe24ad15e012a2a6b43f
Mike Goldsmth <goldsmith.mike@gmail.com>
2022-12-14 11:24:08 +0000

    use is operator when evaluating None, True or False in tests

--- tests/test_options.py
+++ tests/test_options.py
@@ -28,14 +28,14 @@ CUSTOM_ENDPOINT = "localhost:4317"
 
 def test_defaults(monkeypatch):
     options = HoneycombOptions()
-    assert options.traces_apikey == None
-    assert options.metrics_apikey == None
+    assert options.traces_apikey is None
+    assert options.metrics_apikey is None
     assert options.traces_endpoint == "api.honeycomb.io:443"
     assert options.metrics_endpoint == "api.honeycomb.io:443"
     assert options.service_name == "unknown_service:python"
-    assert options.dataset == None
-    assert options.metrics_dataset == None
-    assert options.enable_local_visualizations == False
+    assert options.dataset is None
+    assert options.metrics_dataset is None
+    assert options.enable_local_visualizations is False
 
 
 def test_can_set_service_name_with_param(monkeypatch):
@@ -146,72 +146,72 @@ def test_can_set_sample_rate_with_envvar(monkeypatch):
 
 def test_default_debug_is_false():
     options = HoneycombOptions()
-    assert options.debug == False
+    assert options.debug is False
 
 
 def test_can_set_debug_with_param():
     options = HoneycombOptions(debug=True)
-    assert options.debug == True
+    assert options.debug is True
 
 
 def test_can_set_debug_with_envvar(monkeypatch):
     monkeypatch.setenv(DEBUG, "TRUE")
     options = HoneycombOptions()
-    assert options.debug == True
+    assert options.debug is True
 
 
 def test_traces_endpoint_insecure_defaults_to_false():
     options = HoneycombOptions()
-    assert options.traces_endpoint_insecure == False
+    assert options.traces_endpoint_insecure is False
 
 
 def test_can_set_traces_insecure_with_generic_param():
     options = HoneycombOptions(endpoint_insecure=True)
-    assert options.traces_endpoint_insecure == True
+    assert options.traces_endpoint_insecure is True
 
 
 def test_can_set_traces_insecure_with_traces_param():
     options = HoneycombOptions(traces_endpoint_insecure=True)
-    assert options.traces_endpoint_insecure == True
+    assert options.traces_endpoint_insecure is True
 
 
 def test_can_set_traces_insecure_with_generic_envvar(monkeypatch):
     monkeypatch.setenv(OTEL_EXPORTER_OTLP_INSECURE, "TRUE")
     options = HoneycombOptions()
-    assert options.traces_endpoint_insecure == True
+    assert options.traces_endpoint_insecure is True
 
 
 def test_can_set_traces_insecure_with_traces_envvar(monkeypatch):
     monkeypatch.setenv(OTEL_EXPORTER_OTLP_TRACES_INSECURE, "TRUE")
     options = HoneycombOptions()
-    assert options.traces_endpoint_insecure == True
+    assert options.traces_endpoint_insecure is True
 
 
 def test_metrics_endpoint_insecure_defaults_to_false():
     options = HoneycombOptions()
-    assert options.metrics_endpoint_insecure == False
+    assert options.metrics_endpoint_insecure is False
 
 
 def test_can_set_metrics_insecure_with_generic_param():
     options = HoneycombOptions(endpoint_insecure=True)
-    assert options.metrics_endpoint_insecure == True
+    assert options.metrics_endpoint_insecure is True
 
 
 def test_can_set_metrics_insecure_with_traces_param():
     options = HoneycombOptions(metrics_endpoint_insecure=True)
-    assert options.metrics_endpoint_insecure == True
+    assert options.metrics_endpoint_insecure is True
 
 
 def test_can_set_metrics_insecure_with_generic_envvar(monkeypatch):
     monkeypatch.setenv(OTEL_EXPORTER_OTLP_INSECURE, "TRUE")
     options = HoneycombOptions()
-    assert options.metrics_endpoint_insecure == True
+    assert options.metrics_endpoint_insecure is True
 
 
 def test_can_set_metrics_insecure_with_traces_envvar(monkeypatch):
     monkeypatch.setenv(OTEL_EXPORTER_OTLP_METRICS_INSECURE, "TRUE")
     options = HoneycombOptions()
-    assert options.metrics_endpoint_insecure == True
+    assert options.metrics_endpoint_insecure is True
 
 
 def test_can_set_dataset_with_param():
@@ -238,10 +238,10 @@ def test_can_set_metrics_dataset_with_envvar(monkeypatch):
 
 def test_can_set_enable_local_visualizations_with_param():
     options = HoneycombOptions(enable_local_visualizations=True)
-    assert options.enable_local_visualizations == True
+    assert options.enable_local_visualizations is True
 
 
 def test_can_set_enable_local_visualizations_with_envvare(monkeypatch):
     monkeypatch.setenv(HONEYCOMB_ENABLE_LOCAL_VISUALIZATIONS, "TRUE")
     options = HoneycombOptions()
-    assert options.enable_local_visualizations == True
+    assert options.enable_local_visualizations is True
