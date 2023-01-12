00845d92c027414fc002c6b58f71c873934fefd8
Emily Ashley <15912063+emilyashley@users.noreply.github.com>
2023-01-11 18:58:25 -0600

    Testing sampler, add tests for options as zero

--- tests/test_sampler.py
+++ tests/test_sampler.py
@@ -1,10 +1,50 @@
-# from honeycomb.opentelemetry.options import HoneycombOptions
-# from honeycomb.opentelemetry.sampler import configure_sampler
+from honeycomb.opentelemetry.options import HoneycombOptions
+from honeycomb.opentelemetry.sampler import configure_sampler, DeterministicSampler
 
-# TODO: sampler with rate of undefined configures inner ALWAYS_ON
+from opentelemetry.sdk.trace.sampling import (
+    DEFAULT_OFF,
+    DEFAULT_ON,
+    ParentBasedTraceIdRatio
+#     SamplingDecision,
+#     SamplingResult
+)
 
-# TODO: sampler with rate of 1 configures inner ALWAYS_ON
 
-# TODO: sampler with rate of 0 configures inner ALWAYS_OFF
+def test_undefined_defaults_to_DEFAULT_ON():
+    deterministic_sampler = configure_sampler()
+    assert isinstance(deterministic_sampler, DeterministicSampler)
+    assert deterministic_sampler.rate == 1
+    assert deterministic_sampler._sampler == DEFAULT_ON
 
-# TODO: sampler with rate of 10 configures inner TraceIdRatioBased sampler with a ratio of 0.1
+    # TODO: get result decision, assert RECORD_AND_SAMPLED, 
+    # assert sample rate attribute
+
+
+def test_sampler_with_rate_of_1_defaults_to_DEFAULT_ON():
+    deterministic_sampler = configure_sampler(HoneycombOptions(sample_rate=1))
+    assert isinstance(deterministic_sampler, DeterministicSampler)
+    assert deterministic_sampler.rate == 1
+    assert deterministic_sampler._sampler == DEFAULT_ON
+
+    # TODO: get result decision, assert RECORD_AND_SAMPLED
+    # assert sample rate attribute
+
+# SEE: failing options tests for setting 0 as a rate.
+# def test_sampler_with_rate_of_0_defaults_to_DEFAULT_OFF():
+#     options = HoneycombOptions(sample_rate=0)
+#     deterministic_sampler = configure_sampler(options)
+#     assert isinstance(deterministic_sampler, DeterministicSampler)
+#     assert deterministic_sampler.rate == 0
+#     assert deterministic_sampler._sampler == DEFAULT_OFF
+
+
+def test_sampler_with_rate_of_10_configures_ParentBasedTraceIdRatio():
+    deterministic_sampler = configure_sampler(HoneycombOptions(sample_rate=10))
+    assert isinstance(deterministic_sampler, DeterministicSampler)
+    assert deterministic_sampler.rate == 10
+    assert isinstance(deterministic_sampler._sampler, ParentBasedTraceIdRatio)
+
+    # TODO: test result decisions
+    # assert sample rate attribute
+
+# TODO: add a test that our custom sample rate has been added AND other attributes are intact?
