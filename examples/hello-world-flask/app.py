b932b6f48097f8a4d76d6155db46c66d707bd9aa
JamieDanielson <jamieedanielson@gmail.com>
2023-01-19 13:33:35 -0500

    add config example to example flask app

--- examples/hello-world-flask/app.py
+++ examples/hello-world-flask/app.py
@@ -1,5 +1,20 @@
+import os
 from flask import Flask
 from opentelemetry import trace
+from honeycomb.opentelemetry import configure_opentelemetry, HoneycombOptions
+
+configure_opentelemetry(
+    HoneycombOptions(
+        debug=True,
+        apikey=os.environ.get("HONEYCOMB_API_KEY"),
+        service_name="otel-python-example"
+    )
+)
+
+# or use environment variables instead of configure_opentelemetry
+# export DEBUG=true
+# export HONEYCOMB_API_KEY=abc123
+# export OTEL_SERVICE_NAME=otel-python-example
 
 app = Flask(__name__)
 tracer = trace.get_tracer(__name__)
