c8342d880964af7b3cb5c9cbea668d49803cad0a
Emily Ashley <15912063+emilyashley@users.noreply.github.com>
2023-01-19 20:56:51 -0600

    Initialize the simplespanprocessor and on_start calls

--- src/honeycomb/opentelemetry/baggage.py
+++ src/honeycomb/opentelemetry/baggage.py
@@ -1,4 +1,14 @@
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import (
+    BatchSpanProcessor,
+    SimpleSpanProcessor,
+    SpanExporter
+)
+from opentelemetry.trace import Span
+from opentelemetry.context import Context
+
+# spanprocessor, trace.
+# spanexporter, trace.export
+# simplespanprocessor, trace.export
 
 class BatchWithBaggageSpanProcessor(BatchSpanProcessor):
     # https://opentelemetry-python.readthedocs.io/en/latest/sdk/trace.export.html#opentelemetry.sdk.trace.export.BatchSpanProcessor
@@ -7,9 +17,52 @@ class BatchWithBaggageSpanProcessor(BatchSpanProcessor):
     addition of BaggageSpanProcessor behavior.
     """
 
-    def __init__(self, exporter) -> None:
-        super().__init__(exporter)
+    def __init__(
+        self, 
+        span_exporter: SpanExporter
+        ) -> None:
+        super().__init__(span_exporter)
+        self.bsp = BaggageSpanProcessor()
 
-    # TODO: BaggageSpanProcessor on start? 
-    # a span being started
-    # parent context in which the span was started
+    def on_start(
+        self,
+        span: Span,
+        parent_context: Context
+    ) -> None:
+        self.bsp.on_start(span, parent_context)
+
+
+class BaggageSpanProcessor(SimpleSpanProcessor):
+    """
+     The BaggageSpanProcessor reads entries stored in Baggage
+     from the parent context and adds the baggage entries' keys and
+     values to the span as attributes on span start.
+
+     Add this span processor to a tracer provider.
+
+     Keys and values added to Baggage will appear on subsequent child
+     spans for a trace within this service *and* be propagated to external
+     services in accordance with any configured propagation formats
+     configured. If the external services also have a Baggage span
+     processor, the keys and values will appear in those child spans as
+     well.
+
+     ⚠ Warning ⚠️
+
+     Do not put sensitive information in Baggage.
+
+     To repeat: a consequence of adding data to Baggage is that the keys and
+     values will appear in all outgoing HTTP headers from the application.
+
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    # TODO: on start 
+    #  span being started
+    #  parent context, the context in which span was started
+
+    # propagate baggage from parent context
+    #  get all entries
+    # for each entry, set them as span attributes
