143b5756dbed4aa7c1ab0500d46fd9dfb8de61dc
Emily Ashley <15912063+emilyashley@users.noreply.github.com>
2023-01-19 22:17:39 -0600

    Add baggage to example app
    used: https://opentelemetry.io/docs/instrumentation/python/cookbook/#capturing-baggage-at-different-contexts

--- examples/hello-world-flask/app.py
+++ examples/hello-world-flask/app.py
@@ -1,12 +1,14 @@
 from flask import Flask
-from opentelemetry import trace
+from opentelemetry import trace, baggage
 
 app = Flask(__name__)
 tracer = trace.get_tracer(__name__)
 
 @app.route("/")
 def hello_world():
-    with tracer.start_as_current_span("foo"):
-        with tracer.start_as_current_span("bar"):
+    with tracer.start_as_current_span(name="foo"):
+        parent_ctx = baggage.set_baggage("context", "parent")
+        with tracer.start_as_current_span(name="bar", context=parent_ctx):
+            baggage.set_baggage("context", "child")
             print("baz")
     return "Hello World"
