a650b25614f4a52acd9e200f9bab831efe88b21f
JamieDanielson <jamieedanielson@gmail.com>
2023-01-23 15:38:15 -0500

    use env vars, config in code in other example

--- examples/hello-world-flask/app.py
+++ examples/hello-world-flask/app.py
@@ -3,16 +3,7 @@ from flask import Flask
 from opentelemetry import trace
 from honeycomb.opentelemetry import configure_opentelemetry, HoneycombOptions
 
-configure_opentelemetry(
-    HoneycombOptions(
-        debug=True,
-        apikey=os.environ.get("HONEYCOMB_API_KEY"),
-        service_name="otel-python-example"
-    )
-)
-
-# or use environment variables instead of configure_opentelemetry
-# export DEBUG=true
+# use environment variables
 # export HONEYCOMB_API_KEY=abc123
 # export OTEL_SERVICE_NAME=otel-python-example
 
