2cf6e68081e9af8d802f5c465a9c77353a9abe90
Emily Ashley <15912063+emilyashley@users.noreply.github.com>
2023-01-25 21:08:53 -0600

    Attach and detach tokens in example app

--- examples/hello-world-flask/app.py
+++ examples/hello-world-flask/app.py
@@ -1,24 +1,35 @@
 from flask import Flask
 from opentelemetry import trace, baggage
+from opentelemetry.context import attach, detach
 
 app = Flask(__name__)
 tracer = trace.get_tracer(__name__)
 
+#Recommended: use attach and detach tokens for Context management with Baggage
 @app.route("/")
 def hello_world():
-    root_ctx = baggage.set_baggage("whatever", "friend")
-    with tracer.start_as_current_span(name="foo", context=root_ctx):
-                
-        # compare current context to the root ctx that was passed in
+    token = attach(baggage.set_baggage("queen", "bee"))
+ 
+    with tracer.start_as_current_span(name="honey"):
+        token_honey = attach(baggage.set_baggage("honey", "bee"))
 
-
-        parent_ctx = baggage.set_baggage("context", "parent", root_ctx)
-
-        # some more business logic here
-        parent_ctx2 = baggage.set_baggage("additional_context", "parent", parent_ctx)
-        
-        
-        with tracer.start_as_current_span(name="bar", context=parent_ctx2):
-            child_ctx = baggage.set_baggage("context", "child") # this goes nowhere?
-            print("baz")
+        with tracer.start_as_current_span(name="child"):
+            # this goes nowhere if it is the final span in a trace
+            child_token = attach(baggage.set_baggage("bee", "that_maybe_doesnt_propagate"))
+            detach(child_token)
+        detach(token_honey)
+    detach(token)
     return "Hello World"
+
+# For manually passing around the Context for baggage
+@app.route("/ctx")
+def hello_ctx_world():
+    ctx = baggage.set_baggage("worker", "bees")
+    with tracer.start_as_current_span(name="bumble", context=ctx):
+        ctx = baggage.set_baggage("bumble", "bees", ctx)
+        # say you have more business logic before adding additional baggage
+        ctx = baggage.set_baggage("additional", "bees", ctx)
+        with tracer.start_as_current_span(name="last", context=ctx):
+            # this goes nowhere if it is the final span in a trace
+            ctx = baggage.set_baggage("last", "bee", ctx) 
+    return "Hello Context World"
