479ea53ef7a62c2c9c01c9ef044b77769d41f26a
brookesargent <brookesargent@honeycomb.io>
2024-03-05 10:03:17 -0500

    update utils keys

--- tests/utils.py
+++ tests/utils.py
@@ -1,4 +1,4 @@
 # classic keys are 32 chars long
-CLASSIC_APIKEY = "this is a string that is 32 char"
+CLASSIC_APIKEY = "c1a551c1111111111111111111111111"
 # non-classic keys are 22 chars log
-APIKEY = "an api key for 22 char"
+APIKEY = "shinynewenvironmentkey"
